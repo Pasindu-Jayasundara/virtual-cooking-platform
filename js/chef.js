f6e17f187b36dcc28a6888a1018975784ad26d55
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-04-18 19:38:21 +0530

    Update chef.js

--- js/chef.js
+++ js/chef.js
@@ -27,7 +27,7 @@ function loadChefs() {
             var chefCard = `
                 <div class="chef-card" onclick="gotoChefProfile(${card.id});">
     
-                    <div class="chef-card-header" style="background-image: url('/resources/chef/1.jpg');"></div>
+                    <div class="chef-card-header" style="background-image: url(${card.image.replce("..","")});"></div>../resources/chef/1.jpg
 
                     <div class="chef-card-body">
                         <div class="chef-tutorial-wrap">
