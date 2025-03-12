739478ca12ad8d5dbdde38f46d02b8ba313da380
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-03-12 16:44:20 +0530

    update

--- server/src/routes/normal_route.js
+++ server/src/routes/normal_route.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const Chef = require('../models/cheff');
+
 router.get('/home',(req,res)=>{
     res.send('Home route');
 });
 
 router.get('/about',(req,res)=>{
     res.send('About route');
+});
+
+router.get('/chefs',async (req,res)=>{
+    
+    try {
+        const resultJson = await Chef.find({});
+        res.json(resultJson);
+    } catch (error) {
+        console.error("Error fetching chefs:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+    
 });
 
 module.exports = router;
