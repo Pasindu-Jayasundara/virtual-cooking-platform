5615acf6c2a0d8a957fbe8ad77c9969e13c47a15
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-03-14 12:37:15 +0530

    remove unused mongoose imports from auth and normal routes

--- server/src/routes/normal_route.js
+++ server/src/routes/normal_route.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const mongoose = require('mongoose');
 const Chef = require('../models/cheff');
 
 router.get('/home',(req,res)=>{
