d559a2536a4bec43b329364e79d8af8a7294ed84
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-05-20 23:28:13 +0530

    image path change in rending.js

--- js/trending.js
+++ js/trending.js
@@ -22,7 +22,7 @@ function loadTrending() {
                 var card = trendingArray[id];
                 var trendingCard = `
                     <div class="search-card trending-search-card">
-                        <div class="search-card-head" style="background-image:url(${card.image});" onclick="gotolesson(${card.id});">
+                        <div class="search-card-head" style="background-image:url(${card.image.replace("..","/virtual-cooking-platform")});" onclick="gotolesson(${card.id});">
                             <div class="trending-mark">Flash</div>
                         </div>
                         <div class="search-card-body">
